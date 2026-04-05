11d813f85b14311dda78627aa4b6b15b1a8de342
Muhammad Yaseen <casemng@outlook.com>
2026-04-05 21:12:51 +0300

    add front end project and set setting correctly

---
+++ Marilog.Web.Client/Layout/MainLayout.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace Marilog.Web.Client.Layout
+{
+    public partial class MainLayout
+    {
+        private bool drawerOpen = true;
+
+        private void ToggleDrawer()
+        {
+            drawerOpen = !drawerOpen;
+        }
+    }
+}
