8947ebb6bb4a1d38ba0bbe6fcf335cf7f92c9a3b
Muhammad Yaseen <casemng@outlook.com>
2026-04-05 22:23:12 +0300

    add navication controller

---
+++ Marilog.Presentation/Controllers/NavigationController.cs
@@ -0,0 +1,27 @@
+﻿using Marilog.Application.DTOs.Responses;
+using Marilog.Application.Interfaces.FrontendServices;
+using Marilog.Presentation.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Marilog.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/navigation")]
+    public class NavigationController : ControllerBase
+    {
+        private readonly INavigationService _service;
+
+        public NavigationController(INavigationService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse<List<NavItemResponse>>>> Get(CancellationToken ct)
+        {
+            var data = await _service.GetAsync(ct);
+            return Ok(ApiResponse<List<NavItemResponse>>.Ok(data));
+        }
+    }
+}
