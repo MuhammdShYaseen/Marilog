00e4d1ebfb3cc31c47e89f9c5aa6625fc91a9510
Muhammad Yaseen <casemng@outlook.com>
2026-05-07 10:53:57 +0300

    add apptheme dtos

---
+++ Marilog.Contracts/DTOs/Frontend/AppTheme/CreateAppThemeRequest.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Marilog.Contracts.DTOs.Frontend.AppTheme
+{
+    public class CreateAppThemeRequest
+    {
+        public string ThemeName { get; set; } = default!;
+        public string ThemeKey { get; set; } = default!;
+        public bool IsDefault { get; set; }
+        public string PrimaryColor { get; set; } = default!;
+        public string SecondaryColor { get; set; } = default!;
+        public string AppBarColor { get; set; } = default!;
+        public string BackgroundColor { get; set; } = default!;
+        public string SurfaceColor { get; set; } = default!;
+        public string ErrorColor { get; set; } = default!;
+        public string SuccessColor { get; set; } = default!;
+        public string WarningColor { get; set; } = default!;
+        public string FontFamily { get; set; } = default!;
+        public string BaseFontSize { get; set; } = default!;
+        public bool IsDarkMode { get; set; }
+    }
+}
---
+++ Marilog.Contracts/DTOs/Frontend/AppTheme/UpdateAppThemeRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marilog.Contracts.DTOs.Frontend.AppTheme
+{
+    public class UpdateAppThemeRequest
+    {
+        public string? ThemeName { get; set; }
+        public string? PrimaryColor { get; set; }
+        public string? SecondaryColor { get; set; }
+        public string? AppBarColor { get; set; }
+        public string? BackgroundColor { get; set; }
+        public string? SurfaceColor { get; set; }
+        public string? ErrorColor { get; set; }
+        public string? SuccessColor { get; set; }
+        public string? WarningColor { get; set; }
+        public string? FontFamily { get; set; }
+        public string? BaseFontSize { get; set; }
+        public bool? IsDarkMode { get; set; }
+        public bool? IsDefault { get; set; }
+    }
+}
