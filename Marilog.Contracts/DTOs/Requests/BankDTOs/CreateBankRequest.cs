966ec7768d27863a9406863e031d1820d390e848
Muhammad Shekh Yaseen <casemng@gmail.com>
2026-07-12 23:33:59 +0300

    fill Upset Bank dtos

--- Marilog.Contracts/DTOs/Requests/BankDTOs/CreateBankRequest.cs
+++ Marilog.Contracts/DTOs/Requests/BankDTOs/CreateBankRequest.cs
@@ -4,5 +4,30 @@ namespace Marilog.Contracts.DTOs.Requests.BankDTOs
 {
     public class CreateBankRequest
     {
+        public string Name { get; set; } = null!;
+
+        public string? ShortName { get; set; }
+
+        public string? LegalName { get; set; }
+
+        public int? ParentBankId { get; set; }
+
+        public string? SwiftBic { get; set; }
+
+        public string? BranchCode { get; set; }
+
+        public string? ClearingCode { get; set; }
+
+        public string? NationalBankCode { get; set; }
+
+        public int CountryId { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Website { get; set; }
+
+        public string? Note { get; set; }
     }
 }
--- Marilog.Contracts/DTOs/Requests/BankDTOs/UpdateBankRequest.cs
+++ Marilog.Contracts/DTOs/Requests/BankDTOs/UpdateBankRequest.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace Marilog.Contracts.DTOs.Requests.BankDTOs
 {
     public class UpdateBankRequest
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? ShortName { get; set; }
+
+        public string? LegalName { get; set; }
+
+        public int? ParentBankId { get; set; }
+
+        public string? SwiftBic { get; set; }
+
+        public string? BranchCode { get; set; }
+
+        public string? ClearingCode { get; set; }
+
+        public string? NationalBankCode { get; set; }
+
+        public int CountryId { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Website { get; set; }
+
+        public string? Note { get; set; }
     }
 }
