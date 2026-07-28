8aaa6b528d9d4f3f3281f1f3d82748f4df26ddc5
Muhammad Yaseen <casemng@outlook.com>
2026-07-28 13:07:45 +0300

    remove attachment migration

---
+++ Marilog.Infrastructure/Migrations/20260728100620_RemoveAttachmentFromEmail.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marilog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAttachmentFromEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EmailAttachments_Emails_EmailId",
+                table: "EmailAttachments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_EmailAttachments_Emails_EmailId",
+                table: "EmailAttachments",
+                column: "EmailId",
+                principalTable: "Emails",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
