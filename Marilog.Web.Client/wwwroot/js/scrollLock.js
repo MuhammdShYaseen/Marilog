5bb9ebddd87f4c3e19c6d37da85b91f8f77ecdea
Muhammad Yaseen <casemng@outlook.com>
2026-08-17 11:54:31 +0300

    fix swift ui

---
+++ Marilog.Web.Client/wwwroot/js/scrollLock.js
@@ -0,0 +1,21 @@
+﻿window.scrollLock = {
+    _savedScrollY: 0,
+
+    lock: function () {
+        this._savedScrollY = window.scrollY || document.documentElement.scrollTop;
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${this._savedScrollY}px`;
+        document.body.style.left = '0';
+        document.body.style.right = '0';
+        document.body.style.width = '100%';
+    },
+
+    unlock: function () {
+        document.body.style.position = '';
+        document.body.style.top = '';
+        document.body.style.left = '';
+        document.body.style.right = '';
+        document.body.style.width = '';
+        window.scrollTo(0, this._savedScrollY);
+    }
+};
