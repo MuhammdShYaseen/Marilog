db8924787ef3822103bdabff58b7a8f545aff5f3
Muhammad Yaseen <casemng@outlook.com>
2026-09-08 10:25:34 +0300

    add migration Discreption payment

---
+++ Marilog.Infrastructure/Migrations/20260908072425_DiscreptionToPayment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marilog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DiscreptionToPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discreption",
+                table: "payments",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discreption",
+                table: "payments");
+        }
+    }
+}
